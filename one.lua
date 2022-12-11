490be22e6868d914db5a58987e8bf93979296ddf
Daniel Lundin <d-dog@d-dog.se>
2022-12-11 21:34:22 +0100

    day 1

---
+++ one.lua
@@ -0,0 +1,22 @@
+io.input('./input-1.txt')
+
+local calories = 0
+local elf_calories = {}
+
+while true do
+  local line = io.read("*line")
+  if line == nil then
+    table.insert(elf_calories, calories)
+    break 
+  elseif line == '' then
+    table.insert(elf_calories, calories)
+    calories = 0
+  else
+    number = tonumber(line)
+    calories = calories + number
+  end
+end
+
+table.sort(elf_calories, function(a, b) return a > b end);
+
+print('top three total', elf_calories[1] + elf_calories[2] + elf_calories[3])
