c6899b3524b5d7e636733bca2d23492faf08b1ed
Daniel Lundin <d-dog@d-dog.se>
2022-12-15 21:08:07 +0100

    day 8 - part 2

--- eight.lua
+++ eight.lua
@@ -1,45 +1,41 @@
 io.input('./input-8.txt')
 
-forest = {}
+FOREST = {}
 while true do
 	local line = io.read('*line')
 	if line == nil then break end
-	table.insert(forest, line)
+	table.insert(FOREST, line)
 end
 
 
-trees_visible = 0
+TREES_VISIBLE = 0
 
-function free_sight(height, x, y, dx, dy)
-	local row = forest[y]
-	if row == nil then return true end
+local function tree_sight(height, x, y, dx, dy)
+	local row = FOREST[y]
+	if row == nil then return 0 end
 
-	tree = tonumber(row:sub(x,x))
-	if tree == nil then return true end
-	if tree >= height then return false end
+	local tree = tonumber(row:sub(x,x))
+	if tree == nil then return 0 end
+	if tree >= height then return 1 end
 
-	return free_sight(height, x+dx, y+dy, dx, dy)
+	return 1 + tree_sight(height, x+dx, y+dy, dx, dy)
 end
 
-for row, value in ipairs(forest) do
-
+HIGHEST_SCENIC_SCORE = 0
+for row, value in ipairs(FOREST) do
 	for column = 1, #value do
 		local tree = tonumber(value:sub(column, column))
-		if column == 1 or column == #value then
-			trees_visible = trees_visible + 1
-		elseif row == 1 or row == #value then
-			trees_visible = trees_visible + 1
-		else
-			free_sight_up = free_sight(tree, column, row-1, 0, -1)
-			free_sight_down = free_sight(tree, column, row+1, 0, 1)
-			free_sight_left = free_sight(tree, column-1, row, -1, 0)
-			free_sight_right = free_sight(tree, column+1, row, 1, 0)
-			if free_sight_up or free_sight_down or free_sight_left or free_sight_right then
-				trees_visible = trees_visible + 1
-			end
+		local sight_up = tree_sight(tree, column, row-1, 0, -1)
+		local sight_down = tree_sight(tree, column, row+1, 0, 1)
+		local sight_left = tree_sight(tree, column-1, row, -1, 0)
+		local sight_right = tree_sight(tree, column+1, row, 1, 0)
+		local scenic_score = sight_up * sight_down * sight_left * sight_right
+
+		if scenic_score > HIGHEST_SCENIC_SCORE then
+			HIGHEST_SCENIC_SCORE = scenic_score
 		end
 	end
 
 end
 
-print('trees visible', trees_visible)
+print('highest scenic score', HIGHEST_SCENIC_SCORE)
