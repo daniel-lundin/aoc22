2e11897872ccce87150b809acf57628cc688d055
Daniel Lundin <d-dog@d-dog.se>
2022-12-18 22:57:06 +0100

    day 12 - part 1 (djisktra to the rescue)

--- twelve.lua
+++ twelve.lua
@@ -1,4 +1,4 @@
-io.input('./input-12-sample.txt')
+io.input('./input-12.txt')
 
 local mountain = {}
 
@@ -36,38 +36,6 @@ while true do
 	row = row + 1
 end
 
-local function copy(t)
-	local new = {}
-	for i in pairs(t) do
-		new[i] = {}
-		for j in pairs(t[i]) do
-			new[i][j] = t[i][j]
-		end
-	end
-	return new
-end
-
-local function debug(t)
-	for i in pairs(t) do
-		for index, j in pairs(t[i]) do
-			io.write(index .. ',' .. i .. ' ')
-		end
-	end
-	print('')
-end
-
-local function dump_visits(t)
-	for i in ipairs(t) do
-		for _, steps in ipairs(t[i]) do
-			if steps == 1000000 then
-				io.write(' ')
-			else
-				io.write('x')
-			end
-		end
-		print('')
-	end
-end
 
 local function path_possible(x, y, dx, dy)
 	local altitude = mountain[y][x]
@@ -85,75 +53,66 @@ local function path_possible(x, y, dx, dy)
 	return false
 end
 
-local shortest_path = 10000000
-local counter = 1
 
-local function get_altitude(x, y)
-	if mountain[y] == nil then
-		return -1
-	end
-	if mountain[y][x] == nil then
-		return -1
-	end
-	return mountain[y][x]
-end
 
-local function walk(pos_x, pos_y, steps, visits, path, shortest_paths)
-	counter = counter + 1
-	if counter % 10000 == 0 then
-		dump_visits(shortest_paths)
-	end
-	if steps > shortest_path then
-		return
-	end
-	if visits[pos_y][pos_x] == true then
-		return
-	end
-
-	if shortest_paths[pos_y][pos_x] < steps then
-		return
-	end
-	shortest_paths[pos_y][pos_x] = steps
-
-	local new_path  = path .. ' ' .. pos_x .. ',' .. pos_y
-
-	local new_visits = copy(visits)
-	new_visits[pos_y][pos_x] = true
-
-	if pos_x == goal_x and pos_y == goal_y then
-		if steps < shortest_path then
-			shortest_path = steps
-		end
-		return
-	end
-
-	-- prioratize paths with higher altitude
-	local directions = {{1,0}, {-1,0}, {0,1}, {0,-1}}
-
-	table.sort(directions, function(d1, d2)
-		local altitude1 = get_altitude(pos_x + d1[1], pos_y + d1[2])
-		local altitude2 = get_altitude(pos_x + d2[1], pos_y + d2[2])
-		return altitude1 > altitude2
-	end)
-
-	for _, direction in ipairs(directions) do
-		if path_possible(pos_x, pos_y, direction[1], direction[2]) then
-			walk(pos_x + direction[1], pos_y + direction[2], steps + 1, new_visits, new_path, shortest_paths)
-		end
-	end
-end
-
-local visited_squares = {}
 local shortest_paths = {}
+local unvisited = {}
 for i in ipairs(mountain) do
-	visited_squares[i] = {}
 	shortest_paths[i] = {}
 	for j in ipairs(mountain[i]) do
-		shortest_paths[i][j] = 1000000
-		visited_squares[i][j] = false
+		if i == start_position_y and j == start_position_x then
+			shortest_paths[i][j] = 0
+		else
+			shortest_paths[i][j] = 1000000
+		end
+		table.insert(unvisited, {j, i})
 	end
 end
 
-walk(start_position_x, start_position_y, 0, visited_squares, '', shortest_paths)
+local pos_x = start_position_x
+local pos_y = start_position_y
 
-print('shortest path', shortest_path)
+local function find_tuple_index(t, x, y)
+	for i, value in ipairs(t) do
+		if x == value[1] and y == value[2] then
+			return i
+		end
+	end
+end
+
+while not (pos_x == goal_x and pos_y == goal_y) do
+
+	local directions = {{1,0}, {-1,0}, {0,1}, {0,-1}}
+
+	-- update neighbours tentative distances
+	for _, direction in ipairs(directions) do
+		if path_possible(pos_x, pos_y, direction[1], direction[2]) then
+			local distance = shortest_paths[pos_y][pos_x] + 1
+			if distance < shortest_paths[pos_y+direction[2]][pos_x+direction[1]] then
+				shortest_paths[pos_y+direction[2]][pos_x+direction[1]] = distance
+			end
+		end
+	end
+
+	-- remove from unvisited set
+	local unvisited_index = find_tuple_index(unvisited, pos_x, pos_y)
+	table.remove(unvisited, unvisited_index)
+
+	-- find unvisited node with lowest tentative distance
+	local next_node
+	local lowest_distance=100000
+	for _, node in ipairs(unvisited) do
+		if shortest_paths[node[2]][node[1]] < lowest_distance then
+			next_node = node
+			lowest_distance = shortest_paths[node[2]][node[1]]
+		end
+	end
+
+	if next_node == nil then
+		break
+	end
+	pos_x = next_node[1]
+	pos_y = next_node[2]
+end
+
+print('shortest path', shortest_paths[goal_y][goal_x])
