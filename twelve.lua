7b8203e103667f3b2d66ae75ab5e5ce8b2b38216
Daniel Lundin <d-dog@d-dog.se>
2022-12-18 23:11:24 +0100

    day 12 - part 2

--- twelve.lua
+++ twelve.lua
@@ -2,8 +2,6 @@ io.input('./input-12.txt')
 
 local mountain = {}
 
-local start_position_x = 0
-local start_position_y = 0
 local goal_x = 0
 local goal_y = 0
 
@@ -21,8 +19,6 @@ while true do
 	for char in line:gmatch("%S") do
 		if char == 'S' then
 			table.insert(height_line, 1)
-			start_position_x = column
-			start_position_y = row
 		elseif char == 'E' then
 			table.insert(height_line, 26)
 			goal_x = column
@@ -54,24 +50,6 @@ local function path_possible(x, y, dx, dy)
 end
 
 
-
-local shortest_paths = {}
-local unvisited = {}
-for i in ipairs(mountain) do
-	shortest_paths[i] = {}
-	for j in ipairs(mountain[i]) do
-		if i == start_position_y and j == start_position_x then
-			shortest_paths[i][j] = 0
-		else
-			shortest_paths[i][j] = 1000000
-		end
-		table.insert(unvisited, {j, i})
-	end
-end
-
-local pos_x = start_position_x
-local pos_y = start_position_y
-
 local function find_tuple_index(t, x, y)
 	for i, value in ipairs(t) do
 		if x == value[1] and y == value[2] then
@@ -80,39 +58,74 @@ local function find_tuple_index(t, x, y)
 	end
 end
 
-while not (pos_x == goal_x and pos_y == goal_y) do
+local function find_shortest_path(start_x, start_y)
+	local shortest_paths = {}
+	local unvisited = {}
+	for i in ipairs(mountain) do
+		shortest_paths[i] = {}
+		for j in ipairs(mountain[i]) do
+			if i == start_y and j == start_x then
+				shortest_paths[i][j] = 0
+			else
+				shortest_paths[i][j] = 1000000
+			end
+			table.insert(unvisited, {j, i})
+		end
+	end
 
-	local directions = {{1,0}, {-1,0}, {0,1}, {0,-1}}
+	local pos_x = start_x
+	local pos_y = start_y
 
-	-- update neighbours tentative distances
-	for _, direction in ipairs(directions) do
-		if path_possible(pos_x, pos_y, direction[1], direction[2]) then
-			local distance = shortest_paths[pos_y][pos_x] + 1
-			if distance < shortest_paths[pos_y+direction[2]][pos_x+direction[1]] then
-				shortest_paths[pos_y+direction[2]][pos_x+direction[1]] = distance
+	while not (pos_x == goal_x and pos_y == goal_y) do
+
+		local directions = {{1,0}, {-1,0}, {0,1}, {0,-1}}
+
+		-- update neighbours tentative distances
+		for _, direction in ipairs(directions) do
+			if path_possible(pos_x, pos_y, direction[1], direction[2]) then
+				local distance = shortest_paths[pos_y][pos_x] + 1
+				if distance < shortest_paths[pos_y+direction[2]][pos_x+direction[1]] then
+					shortest_paths[pos_y+direction[2]][pos_x+direction[1]] = distance
+				end
+			end
+		end
+
+		-- remove from unvisited set
+		local unvisited_index = find_tuple_index(unvisited, pos_x, pos_y)
+		table.remove(unvisited, unvisited_index)
+
+		-- find unvisited node with lowest tentative distance
+		local next_node
+		local lowest_distance=100000
+		for _, node in ipairs(unvisited) do
+			if shortest_paths[node[2]][node[1]] < lowest_distance then
+				next_node = node
+				lowest_distance = shortest_paths[node[2]][node[1]]
+			end
+		end
+
+		if next_node == nil then
+			break
+		end
+		pos_x = next_node[1]
+		pos_y = next_node[2]
+	end
+
+	return shortest_paths[goal_y][goal_x]
+end
+
+local shortest = 100000
+
+for y, height_line in ipairs(mountain) do
+	for x, height in ipairs(height_line) do
+		if height == 1 then
+			local distance = find_shortest_path(x, y)
+			print('a at ' .. x .. ', ' .. y .. ' distance ' .. distance)
+			if distance < shortest then
+				shortest = distance
 			end
 		end
 	end
-
-	-- remove from unvisited set
-	local unvisited_index = find_tuple_index(unvisited, pos_x, pos_y)
-	table.remove(unvisited, unvisited_index)
-
-	-- find unvisited node with lowest tentative distance
-	local next_node
-	local lowest_distance=100000
-	for _, node in ipairs(unvisited) do
-		if shortest_paths[node[2]][node[1]] < lowest_distance then
-			next_node = node
-			lowest_distance = shortest_paths[node[2]][node[1]]
-		end
-	end
-
-	if next_node == nil then
-		break
-	end
-	pos_x = next_node[1]
-	pos_y = next_node[2]
 end
 
-print('shortest path', shortest_paths[goal_y][goal_x])
+print('shortest', shortest)
