d4e9a591e6dc2c5e01e011019ca18c3c9561b449
Daniel Lundin <d-dog@d-dog.se>
2022-12-28 19:12:38 +0100

    day 17 - part 2

--- seventeen.lua
+++ seventeen.lua
@@ -88,13 +88,7 @@ end
 
 local chamber = {}
 
-print('1', shape_height(rock_shapes[1]))
-print('2', shape_height(rock_shapes[2]))
-print('3', shape_height(rock_shapes[3]))
-print('4', shape_height(rock_shapes[4]))
-print('5', shape_height(rock_shapes[5]))
-
-local function draw_chamber(falling_rock)
+local function draw_chamber(falling_rock, rows)
     local highest_y = 0
 
 
@@ -108,7 +102,11 @@ local function draw_chamber(falling_rock)
 
     highest_y = highest_y + 6
 
-    for i=0,highest_y do
+    local from = 0
+    if rows then
+        from = highest_y - rows
+    end
+    for i=from,highest_y do
         local y = highest_y - i
         local row_string = '|'
         for x=0,6 do
@@ -123,8 +121,6 @@ local function draw_chamber(falling_rock)
         print(row_string .. '|')
      end
      print('+-------+')
-    -- draw falling rock
-    -- draw stopped rocks
 end
 
 local function jet_displace(shape, direction)
@@ -165,79 +161,141 @@ local function apply_gravity(shape)
     return new_shape
 end
 
-local shape_index = 1
-local jetindex = 1
 
-for _=1,2022 do
-    -- heighest rock
-    local highest_ground = 0
+local function simulate(steps, jet_index_start, shape_index_start, checker_func)
+    local jet_index = jet_index_start
+    local shape_index = shape_index_start
+    for i=1,steps do
+        -- heighest rock
+        local highest_ground = 0
+        for x in pairs(chamber) do
+            for y in pairs(chamber[x]) do
+                if y >= highest_ground then
+                    highest_ground = y + 1
+                end
+            end
+        end
+        local spawn_y = highest_ground + 3
+
+        -- spawn rock
+        local falling_rock = {}
+        for x in pairs(rock_shapes[shape_index]) do
+            for y in pairs(rock_shapes[shape_index][x]) do
+                if not falling_rock[x + 2] then
+                    falling_rock[x + 2] = {}
+                end
+                falling_rock[x + 2][y + spawn_y] = true
+            end
+        end
+
+        local abort = false
+        while true do
+            local jet = jets[jet_index]
+            if jet_index == #jets then
+                jet_index = 1
+            else
+                jet_index = jet_index + 1
+            end
+
+            local displaced_rock = jet_displace(falling_rock, jet)
+            local fallen_rock = apply_gravity(displaced_rock)
+
+            if not fallen_rock then
+                for x in pairs(displaced_rock) do
+                    if not chamber[x] then
+                        chamber[x] = {}
+                    end
+                    for y in pairs(displaced_rock[x]) do
+                        chamber[x][y] = true
+                    end
+                end
+
+                if checker_func then
+                    if checker_func(i, jet_index, shape_index) then
+                        abort = true
+                    end
+                end
+                break
+            end
+            falling_rock = fallen_rock
+        end
+
+        if abort then return end
+
+        if shape_index == #rock_shapes then
+            shape_index = 1
+        else
+            shape_index = shape_index + 1
+        end
+    end
+end
+
+local shape_repeat = -1
+local jet_repeat = -1
+local repeat_start_y = -1
+local repeat_height = -1
+local repeat_iteration_start = -1
+local repeat_iterations = -1
+
+simulate(10000, 1, 1, function (iteration, jet_index, shape_index)
+    -- check for repeat
+    local highest_y = 0
     for x in pairs(chamber) do
         for y in pairs(chamber[x]) do
-            if y >= highest_ground then
-                highest_ground = y + 1
+            if y > highest_y then
+                highest_y = y
             end
         end
     end
-    local spawn_y = highest_ground + 3
-
-    -- spawn rock
-    local falling_rock = {}
-    for x in pairs(rock_shapes[shape_index]) do
-        for y in pairs(rock_shapes[shape_index][x]) do
-            if not falling_rock[x + 2] then
-               falling_rock[x + 2] = {}
-            end
-            falling_rock[x + 2][y + spawn_y] = true
+    local fills = 0
+    for x=0,6 do
+        if chamber[x] and chamber[x][highest_y] then
+            fills = fills + 1
         end
     end
-
-    -- draw_chamber(falling_rock)
-    while true do
-        local jet = jets[jetindex]
-        if jetindex == #jets then
-            jetindex = 1
+    if fills == 7 then
+        print('full row at', iteration, jet_index, shape_index)
+        if jet_repeat == -1 then
+            jet_repeat = jet_index
+            shape_repeat = shape_index
+            repeat_start_y = highest_y
+            repeat_iteration_start = iteration
         else
-            jetindex = jetindex + 1
-        end
-
-        -- displace
-        -- print('jet push:')
-        local displaced_rock = jet_displace(falling_rock, jet)
-        -- draw_chamber(displaced_rock)
-        -- gravity
-        -- print('falls 1 unit:')
-        local fallen_rock = apply_gravity(displaced_rock)
-        -- draw_chamber(falling_rock)
-        if not fallen_rock then
-            for x in pairs(displaced_rock) do
-                if not chamber[x] then
-                    chamber[x] = {}
-                end
-                for y in pairs(displaced_rock[x]) do
-                    chamber[x][y] = true
-                end
+            if jet_index == jet_repeat and shape_index == shape_repeat then
+                print('found repeat at ', iteration)
+                print('height', highest_y - repeat_start_y)
+                print('repeat iterations', iteration - repeat_iteration_start)
+                repeat_iterations = iteration - repeat_iteration_start
+                repeat_height = highest_y - repeat_start_y
+                return true
             end
-            -- draw_chamber()
-            break
         end
-        falling_rock = fallen_rock
-        -- draw_chamber(falling_rock)
     end
+end)
 
-    if shape_index == #rock_shapes then
-        shape_index = 1
-    else
-        shape_index = shape_index + 1
-    end
+local simulations = 1000000000000
+
+local left_overs = (simulations - repeat_iteration_start) % repeat_iterations
+local repeats = math.floor((simulations - repeat_iteration_start) / repeat_iterations)
+
+-- simulate rest
+chamber = {}
+if shape_repeat == #rock_shapes then
+    shape_repeat = 1
+else
+    shape_repeat = shape_repeat + 1
 end
+simulate(left_overs, jet_repeat, shape_repeat)
 
-local highest_ground = 0
+local highest_y = 0
+
 for x in pairs(chamber) do
     for y in pairs(chamber[x]) do
-        if y >= highest_ground then
-            highest_ground = y + 1
+        if y > highest_y then
+            highest_y = y
         end
     end
 end
 
-print('highest ground', highest_ground)
+print('highest_y', highest_y)
+print('total height', (repeat_start_y + 1) + repeats * repeat_height + (highest_y + 1))
